6ca6a2f4d5dbd56e7edf25401da2a025e7a4aecd
Cameron Sellers <cameronsmac@Camerons-MacBook-Pro.local>
2020-05-12 16:17:30 -0600

    landing card structure made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import LandingPage from "./components/landingPage/landingPage";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faCheckSquare,
+  faCoffee,
+  faCookieBite
+} from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
+library.add(faCheckSquare, faCoffee, faCookieBite);
 
 function App() {
   return (
--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -1,36 +1,36 @@
 import React from "react";
 import { Card } from "reactstrap";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./landingPage.css";
 const LandingPage = () => {
   return (
     <div>
       <div>
         <h1 className="main_header"> New Dads</h1>
+        <div>
+          <FontAwesomeIcon icon="Cookie-Bite" />
+        </div>
       </div>
-      <Card className="cards">
-        <h1>Sleep</h1>
+      <Card className="cards" id="sleep-card">
+        <h1 className="landing-headings">Sleep</h1>
       </Card>
-      <Card className="cards">
-        <h1>Health</h1>
+      <Card className="cards" id="health-card">
+        <h1 className="landing-headings">Health</h1>
       </Card>
-      <Card className="cards">
-        <h1>At Home</h1>
+      <Card className="cards" id="home-card">
+        <h1 className="landing-headings">At Home</h1>
       </Card>
-      <Card className="cards">
-        <h1>Nutrition</h1>
+      <Card className="cards" id="travel-card">
+        <h1 className="landing-headings">Travel</h1>
       </Card>
-      <Card className="cards">
-        <h1>Travel</h1>
+      <Card className="cards" id="tech-card">
+        <h1 className="landing-headings">Helpful Tech</h1>
       </Card>
-      <Card className="cards">
-        <h1>Helpful Tech</h1>
+      <Card className="cards" id="wish-card">
+        <h1 className="landing-headings">What I Wish I Knew</h1>
       </Card>
-      <Card className="cards">
-        <h1>What I Wish I Knew</h1>
-      </Card>
-      <Card className="cards">
-        <h1>Recipes</h1>
+      <Card className="cards" id="recipe-card">
+        <h1 className="landing-headings">Recipes</h1>
       </Card>
     </div>
   );
